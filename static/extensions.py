4dd0ef294463efaf340b55db0032dc59fe7f5356
ads-freed <57936630+ads-freed@users.noreply.github.com>
2025-03-27 10:26:58 +0800

    Delete static/extensions.py

--- static/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
-from flask_socketio import SocketIO
-
-db = SQLAlchemy()
-migrate = Migrate()
-login = LoginManager()
-socketio = SocketIO()
